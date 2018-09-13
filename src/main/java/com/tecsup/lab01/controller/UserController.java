21508e9accbc338127b001bc4ad5db0e708b4be9
jfabiant <rronaldo_jhon_20@hotmail.com>
2018-09-13 12:01:47 -0500

    Cambio final

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -4,15 +4,18 @@ import com.tecsup.lab01.model.User;
 
 public class UserController {
 	
-	User usuario=new User();
+	User usuario = new User();
 	
 	public String insertarnombre(String nombres) {
-        return("El nombre es: "+nombres);
+		usuario.setNombres(nombres);
+        return("El nombre es: "+usuario.getNombres());
 	}
 	public String insertarapellido(String apellido) {
-        return("Su apellido paterno es: "+apellido);
+		usuario.setApelldioPaterno(apellido);
+        return("Su apellido paterno es: "+usuario.getApelldioPaterno());
 	}
 	public String insertaredad(int edad) {
-        return("Su edad es: "+edad);
+		usuario.setEdad(edad);
+        return("Su edad es: "+usuario.getEdad());
 	}
 }
--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -7,7 +7,9 @@ public class UserView {
 	public static void main( String[] args )
     {
 		UserController usrController = new UserController();
-		System.out.println(usrController.insertarnombre("Juan Marcos"));
+		System.out.println(usrController.insertarnombre("Julio Carlos"));
+		System.out.println(usrController.insertarapellido("Barranco Torres"));
+		System.out.println(usrController.insertaredad(18));
     }
 	
 }
