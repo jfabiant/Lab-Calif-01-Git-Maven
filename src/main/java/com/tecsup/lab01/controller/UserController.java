72a819174bae67c00841203348d1e1e7da42cf95
Alumno <Alumno@LAB402-16>
2018-09-13 11:40:26 -0500

    octavo cambio

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -3,9 +3,12 @@ package com.tecsup.lab01.controller;
 import com.tecsup.lab01.model.User;
 
 public class UserController {
-	
-          User usuario=new User();
-	
-	
-
+	User usuario=new User();
+	String nombres="Marco Antonio";
+	public String insertarnombre(String nombres) {
+        
+        nombres=this.nombres;
+        return("El nombre es:"+nombres);  	
+		
+	}
 }
